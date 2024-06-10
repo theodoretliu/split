8f48bdb3346f0608b0df1259170045d0ffec2793
Theodore Liu <theodoretliu@gmail.com>
2024-06-09 23:02:04 -0700

    Delete frontend

--- frontend/app/entry.client.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = require("@remix-run/react");
-const react_dom_1 = require("react-dom");
-(0, react_dom_1.hydrate)(React.createElement(react_1.RemixBrowser, null), document);
